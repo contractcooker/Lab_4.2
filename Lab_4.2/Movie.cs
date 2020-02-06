d675367c0dd138bae9a489441f0f8b2d1880cf4c
Thomas Barron <barron.tom@gmail.com>
2020-02-06 17:29:09 -0500

    Code complete

--- Lab_4.2/Movie.cs
+++ Lab_4.2/Movie.cs
@@ -3,8 +3,25 @@ namespace Lab_4._2
 {
     public class Movie
     {
-        public Movie()
+        private string title;
+        private string genre;
+
+        
+
+        public string Title { get => title; set => title = value; }
+        public string Genre { get => genre; set => genre = value; }
+
+
+
+        public Movie(string title, string genre)
         {
+            this.title = title;
+            this.genre = genre;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0,16} {1,8}", title, genre);
         }
     }
 }
--- Lab_4.2/Program.cs
+++ Lab_4.2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lab_4._2
 {
@@ -6,7 +7,66 @@ namespace Lab_4._2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input;
+            bool flag = true;
+
+
+            List<Movie> movieList = new List<Movie>();
+            movieList.Add(new Movie("Cowboy BeBop", "Sci-Fi"));
+            movieList.Add(new Movie("Interstella 5555", "Sci-Fi"));
+            movieList.Add(new Movie("Serenity", "Sci-Fi"));
+            movieList.Add(new Movie("Animatrix, The", "Sci-Fi"));
+            movieList.Add(new Movie("Pulp Fiction", "Drama"));
+            movieList.Add(new Movie("Resevoir Dogs", "Drama"));
+            movieList.Add(new Movie("Memento", "Mystery"));
+            movieList.Add(new Movie("The Game", "Mystery"));
+            movieList.Add(new Movie("Kill Bill: Vol 1", "Action"));
+            movieList.Add(new Movie("Kill Bill: Vol 2", "Action"));
+
+            Console.WriteLine("Welcome to the Movie List Application!");
+            Console.WriteLine();
+            Console.WriteLine($"There are {movieList.Count} movies in this list");
+            do
+            {
+                /**/
+                Console.Write("What category are you interested in? ");
+                input = Console.ReadLine();
+                foreach (Movie m in movieList)
+                {
+                    if (m.Genre.Contains(input))
+                    {
+                        Console.WriteLine(m);
+                    }
+                }
+                flag = GoAgain();
+
+            } while (flag);
+
+        }
+
+        private static bool GoAgain()
+        {
+            char input = ' ';
+
+            while(!"YyNn".Contains(input))
+            {
+                /**/
+                Console.Write("Continue? (y/n) ");
+                input = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                if ("YyNn".Contains(input))
+                {
+                    break;
+                }
+                Console.WriteLine("Invalid Input!");
+
+            } 
+            if ("Nn".Contains(input))
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
